0b26e39a59f51e4ffb4a0f1b32aed1b9f8ac339b
uhsog <uhsog.tech@gmail.com>
2018-08-26 12:09:50 +0900

    Insert data

--- insert.js
+++ insert.js
@@ -1,18 +1,44 @@
 const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('example.sqlite');  // SQLite の DB ファイル名
+const db = new sqlite.Database('kamakura_tourism.sqlite');  // SQLite の DB ファイル名
 
 // SQL を同期的に実行する
 db.serialize(() => {
   // テーブルがなければ作成する
-  db.run('CREATE TABLE IF NOT EXISTS user (name TEXT, age INTEGER)');
-  
+  db.run('CREATE TABLE IF NOT EXISTS spots (id INTEGER PRIMARY KEY, name TEXT, beacon_id TEXT, detail TEXT)');
+
   // Prepared Statement でデータを挿入する
-  const stmt = db.prepare('INSERT INTO user VALUES (?, ?)');
-  stmt.run(['Foo', 25]);
-  stmt.run(['Bar', 39]);
-  stmt.run(['Baz', 31]);
-  
+  let stmt = db.prepare('INSERT INTO spots VALUES (?, ?, ?, ?)');
+  stmt.run([1, '鎌倉大仏', 'beaconid', 'detail',]);
+
   // prepare() で取得した Prepared Statement オブジェクトをクローズする。これをコールしないとエラーになる
+  stmt.finalize();
+
+  db.run('CREATE TABLE IF NOT EXISTS stations (id INTEGER PRIMARY KEY, name TEXT, beacon_id TEXT)');
+  // Prepared Statement でデータを挿入する
+  stmt = db.prepare('INSERT INTO stations VALUES (?, ?, ?)');
+  stmt.run([1, '鎌倉駅', '']);
+
+  stmt.finalize();
+
+  db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, spot_id INTEGER, alert_at TEXT, name TEXT, beacon_id TEXT)');
+  // Prepared Statement でデータを挿入する
+  stmt = db.prepare('INSERT INTO users VALUES (?, ?, ?, ?, ?)');
+  stmt.run([1, 1, '18:00', 'しげる', 'beacon_id']);
+
+  stmt.finalize();
+
+  db.run('CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT, price INTEGER, station_id INTEGER)');
+  // Prepared Statement でデータを挿入する
+  stmt = db.prepare('INSERT INTO items VALUES (?, ?, ?, ?)');
+  stmt.run([1, 1, 'お土産', 1]);
+
+  stmt.finalize();
+
+  db.run('CREATE TABLE IF NOT EXISTS user_items (id INTEGER PRIMARY KEY, user_id INTEGER, item_id INTEGER, count INTEGER)');
+  // Prepared Statement でデータを挿入する
+  stmt = db.prepare('INSERT INTO user_items VALUES (?, ?, ?, ?)');
+  stmt.run([1, 1, 1, 0]);
+
   stmt.finalize();
 });
 
--- select.js
+++ select.js
@@ -2,12 +2,12 @@ const sqlite = require('sqlite3').verbose();
 const db = new sqlite.Database('example.sqlite');
 
 db.serialize(() => {
-  db.each('SELECT * FROM user', (error, row) => {
+  db.each('SELECT * FROM users', (error, row) => {
     if(error) {
       console.error('Error!', error);
       return;
     }
-    
+
     // カラムを指定してデータを表示する
     console.log(row.name + ' … ' + row.age);
   });
